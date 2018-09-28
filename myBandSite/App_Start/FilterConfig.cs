b69d5c1d95d5667e48b060c8ecdf5efa5dab46ac
Ima Dev <msdn@benko.com>
2018-09-27 17:01:30 -0700

    Add project files.

---
+++ myBandSite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace myBandSite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
